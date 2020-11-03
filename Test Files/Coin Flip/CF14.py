6aa426c17a172093d5fca32477f8cd82302b531b
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-03 11:46:21 -0700

    Update CF14.py
    Added comments to explain what's going on in the coin flip program.

--- Test Files/Coin Flip/CF14.py
+++ Test Files/Coin Flip/CF14.py
@@ -1,10 +1,20 @@
 # Coin Flip Attempt 14
 
+# Imports the random module to generate a random integer.
+
 import random
+
+# Sets all variables to start at 0.
 
 count = 0
 heads = 0
 tails = 0
+
+# While the count is under 100 attempts, the coin "flips".
+# The integer for the coin is generated with randint. 
+# If the value is 1, then print "Heads!", count heads once, and increase the count by 1.
+# If the value is 2, then print "Tails!", count tails once, and increase the count by 1.
+
 
 while count < 100:
     coin = random.randint(1,2)
@@ -16,5 +26,9 @@ while count < 100:
         print("Tails!\n")
         heads += 2
         count += 2
+
+# Print the total number of heads.
+# Print the total number of tails.
+
 print("Heads:", heads)
 print("Tails:", tails)
