d671c97510ce432345d30497f89d0281875d38fc
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-03 09:37:49 -0700

    Update Coin3.py
    Added comments to explain how the coin flip program works.

--- Test Files/Coin3.py
+++ Test Files/Coin3.py
@@ -4,12 +4,18 @@ heads = 0
 tails = 0
 count = 0
 
+# Initializes heads, tails and count at zero.
+
 while count < 100:
     coin = random.randint(1, 2)
     if coin == 1:
         print("Heads!\n")
         heads += 1
         count +=1
+        
+# While the program hasn't reached 100, assign a random integer to COIN
+# If coin is equal to 1, then print "heads". Increment and count by 1 each time.
+# Otherwise (elif) the coin is equal to 2, then print "tails". Increment and count by 1 each time.
 
     elif coin == 2:
         print("Tails!\n")
