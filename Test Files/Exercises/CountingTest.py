3409fa350c4be5f5c4696f16ece69aa6198e1df6
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-03 16:01:11 -0700

    Playing with Counter
    Customized and tweaked settings to understand how this FOR loop works.

---
+++ Test Files/Exercises/CountingTest.py
@@ -0,0 +1,15 @@
+# Counting Test
+
+print("Counting:")
+for i in range(10):
+    print(i, end=" ")
+
+print("\n\nCounting by 10 to 50:")
+for i in range(0, 50, 10):
+    print(i, end=" ")
+
+print("\n\nCounting backwards:")
+for i in range(50, 0, -5):
+    print(i, end=" ")
+
+input("\n\nPress the enter key to exit.\n")
--- chapter04/counter.py
+++ chapter04/counter.py
@@ -1,16 +1,24 @@
 # Counter
 # Demonstrates the range() function
 
+# Prints "Counting:" and numbers 0-10
+
 print("Counting:")
 for i in range(10):
     print(i, end=" ")
+
+# Prints "Counting by fives:" and then numbers 0-50 in increments of 5
 
 print("\n\nCounting by fives:")
 for i in range(0, 50, 5):
     print(i, end=" ")
 
+# Prints "Counting backwards:" and then numbers 10-0, subtracting each time
+
 print("\n\nCounting backwards:")
 for i in range(10, 0, -1):
     print(i, end=" ")
+
+# Ends the program
 
 input("\n\nPress the enter key to exit.\n")
