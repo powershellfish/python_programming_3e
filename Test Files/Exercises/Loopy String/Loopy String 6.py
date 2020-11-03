915c76f33988b87181cfba9f79bed339ac85becb
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-03 15:41:34 -0700

    Practicing reptition

---
+++ Test Files/Exercises/Loopy String/Loopy String 6.py
@@ -0,0 +1,9 @@
+# Loopy String
+
+word = input("Tell me a word you want me to spell out.")
+print("\nHere is what that looks like:")
+
+for letter in word:
+    print (letter)
+
+print("Goodbye!")
---
+++ Test Files/Loopy String 5.py
@@ -0,0 +1,14 @@
+# Loopy String Thing 5
+
+word = input("Enter the word you want to see spelled out:")
+
+# Takes the word the user enters and assigns it to the variable "word"
+
+print("\nHere is what it looks like:")
+
+for letter in word:
+	print (letter)
+
+# Uses a for loop to print out each letter from the sequence (word) on individual lines.
+
+print("\nPress Enter to exit.")
