e4b10edd34a5e243fd02a086126cbb392a69f967
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-12 15:14:31 -0700

    Update Latte.py
    Updated latte with "coffee".

--- Test Files/Latte.py
+++ Test Files/Latte.py
@@ -16,3 +16,21 @@ print(word[3])
 
 word = "latte"
 print(word[4])
+
+print("Stop!")
+
+word2 = "coffee"
+print(word2[-1])
+
+word2 = "coffee"
+print(word2[-2])
+
+word2 = "coffee"
+print(word2[-3])
+
+word2 = "coffee"
+print(word2[-4])
+
+word2 = "coffee"
+print(word2[-5])
+
