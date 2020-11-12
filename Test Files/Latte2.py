d86f2f7658bf634cf978af54ce9f5e01206971fb
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-12 15:46:20 -0700

    Updated random access with notes

---
+++ Test Files/Latte2.py
@@ -0,0 +1,9 @@
+# Testing Latte
+
+import random
+
+word = "latte"
+print("The word is: ", word, "\n")
+
+high = len(word)
+low = -len(word)
--- chapter04/random_access.py
+++ chapter04/random_access.py
@@ -1,13 +1,24 @@
 # Random Access
 # Demonstrates string indexing
 
+# Imports the random module
+
 import random
+
+# Prints the text: The word is: latte
 
 word = "latte"
 print("The word is: ", word, "\n")
 
+# Create two endpoints so random.randrange() can choose a random integer to
+# represent the letter.
+# len(word) means "the highest positive int corresponding to any letter in this string"
+# -len(word means "the lowest int corresponding to any letter in this string"
+
 high = len(word)
 low = -len(word)
+
+# This for loop
 
 for i in range(10):
     position = random.randrange(low, high)
