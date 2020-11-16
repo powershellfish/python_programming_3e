4615349b6a67065092a904c6ca223764a620af18
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-16 15:28:45 -0700

    Created indexing.py
    Playing around with string immutability. Also looking at indexing and positions.

---
+++ Test Files/Indexing.py
@@ -0,0 +1,12 @@
+# Working with indexing and strings
+
+word = "String"
+print(word[0])
+print(word[1])
+print(word[2])
+print(word[3])
+print(word[4])
+print(word[5])
+print("\n")
+
+print(word.upper())
