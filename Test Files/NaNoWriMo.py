6f4933b69666a37e9698e0db2e73b02b9499644c
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-17 11:43:47 -0700

    Added new files to repo
    Biggest update here is Stringcrease. I wanted to print a string based on something the user entered as input, but didn't need the loop to contain IF or ELIF statements. The string prints whatever text the user enters letter by letter, concatenating one character for each line until it reaches the end of the string.
    
    NaNoWriMo is intended to be a program that calculates your daily word count against the total word count to see how far along you are in the process.

---
+++ Test Files/NaNoWriMo.py
@@ -0,0 +1,17 @@
+#NaNoWriMo Word Count Program
+
+nov1 = (1666*1)
+nov2= (1666*2)
+nov3 = (1666*3)
+wordcount = ""
+date = ""
+
+
+print (1666*1)
+print (1666*2)
+print (1666*3)
+
+wordcount = int(input("What is your wordcount for today?"))
+date = str(input("What is the date today?"))
+
+print(wordcount)
---
+++ Test Files/Stringcrease.py
@@ -0,0 +1,16 @@
+# Progressively increase concatenation in string
+
+message = input("Enter some text: ")
+new_message = ""
+
+print()
+for letter in message:
+    new_message += letter
+    print(new_message)
+
+print(new_message)
+
+print()
+for letter in message:
+    new_message == letter
+    print(new_message)
