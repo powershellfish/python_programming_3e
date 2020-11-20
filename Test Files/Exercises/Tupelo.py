48cfb1d53bd4cf23e9363b73bdf2b228852da199
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-20 15:55:03 -0700

    Create Tupelo.py
    Created my own tuple, told it to print, and then printed individual elements of that tuple.

---
+++ Test Files/Exercises/Tupelo.py
@@ -0,0 +1,21 @@
+# Tuplelo Tuple
+
+# creates an empty tuple
+tupelo = ()
+
+# string values to the tuple
+tupelo = ("honey",
+          "steak",
+          "wine",
+          "wings")
+
+# prints the tuple as is
+print("Here is the tuple as-is:")
+print(tupelo)
+
+# prints each individual element of the tuple
+
+print("\n")
+print("Here is each element in the tuple printed out:")
+for item in tupelo:
+    print(item)
