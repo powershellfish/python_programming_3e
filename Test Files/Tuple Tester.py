65ff266b8390c3ea272211531ddcf9f1cf32d4bd
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-21 13:22:24 -0700

    Update Tuple Tester.py
    Updated to include testing for membership from both groups (imposters AND crewmates), tested slicing, and demonstrated concatenation of tuples by combining both groups and printing to the console.

--- Test Files/Tuple Tester.py
+++ Test Files/Tuple Tester.py
@@ -1,27 +1,67 @@
 # Among Us Tuple Tester
 
-amongus = ("cyan",
-           "pink",
+
+# Creates a tuple with all of the crewmates in Among Us
+crewmates = ("cyan",
            "red",
            "green",
            "blue",
            "yellow",
            "orange",
            "black",
-           "white",
            "purple")
 
-print("\nHere is the list of crewmates and imposters:\n")
 
-for item in amongus:
+# Creates a tuple with the imposters in Among Us
+imposters = ("pink",
+             "white")
+
+# Prints the list of crewmates/imposters in the tuple by using a FOR loop
+
+print("\nHere is the list of crewmates and imposters:\n")
+for item in crewmates:
     print(item)
 
-print("\nThere are", len(amongus), "of us.")
+# Uses the LEN function to print the total number of elements in the tuple amongus
 
-input("\nPress the enter key to learn more.")
+print("\nThere are", len(crewmates), "of us.")
 
-if "pink" in amongus:
-    print("\nglitterbot is sus.")
+print("\nLet's see who's an imposter...")
+input("\nPress Enter to find out...")
 
-if "cyan" in amongus:
+# Uses IF statements to test for membership in crewmates or imposters. 
+
+if "pink" in crewmates:
+    print("\nglitterbot is a crewmate.")
+
+if "cyan" in crewmates:
     print("\ncyan is clearly a crewmate.")
+
+if "white" in imposters:
+    print("\nBut Devon is sus!")
+
+# Demonstrates slicing
+
+start = int(input("\nEnter a number to start a slice: "))
+finish = int(input("\nNow enter a number to finish the slice: "))
+print("crewmates[", start, ":", finish, "] is", end=" ")
+print(crewmates[start:finish])
+
+start = int(input("\nEnter a number to start a slice: "))
+finish = int(input("\nNow enter a number to finish the slice: "))
+print("imposters[", start, ":", finish, "] is", end=" ")
+print(imposters[start:finish])
+
+input("\nPress Enter to see what tuple concatenation looks like:")
+
+# Concatenation
+
+crewmates = ("cyan", "red", "green", "blue", "yellow", "orange", "black", "purple")
+print("You find all the crewmates clutered together. They are:")
+print(crewmates)
+print("You find the imposters who haven't killed anyone yet:")
+crewmates += imposters
+print("The group now consists of:")
+print(crewmates)
+
+input("\nPress Enter to exit this hellscape.")
