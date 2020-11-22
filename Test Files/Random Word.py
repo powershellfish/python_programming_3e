436d47b8cf3232a22e3a9ec0c27a0599e3cad849
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-11-22 15:10:04 -0700

    Create Random Word.py
    Created a program that randomly chooses a word from the list and prints it to the console.

---
+++ Test Files/Random Word.py
@@ -0,0 +1,9 @@
+# This program prints a random word from the WORDS constant.
+
+import random
+
+WORDS = ("mountain", "sunset", "sunrise")
+
+word = random.choice(WORDS)
+
+print (word)
---
+++ chapter04/backwords.py
@@ -0,0 +1,9 @@
+# Printing a string backwards
+
+word = hello
+
+print("Printing backwards...")
+for i in range (10, 0, -1):
+    print(i, end=" ")
+
+input("\n\nPress Enter to exit.")
